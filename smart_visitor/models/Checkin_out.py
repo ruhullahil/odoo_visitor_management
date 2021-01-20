fc1d9b2e1c715bc6c0c1f2d8fc8a886598ed6745
ruhullahil <ruhullahilt@gmail.com>
2021-01-20 17:44:18 +0600

    20-1-21 final

--- smart_visitor/models/Checkin_out.py
+++ smart_visitor/models/Checkin_out.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
+import datetime
+from odoo.exceptions import ValidationError
 
 
 class VisitorCheckInOut(models.Model):
@@ -12,9 +14,42 @@ class VisitorCheckInOut(models.Model):
     #         if rec.is_internal:
     #             rec.visitor_name = self.env['smart_visitor'].search([('is_internal', '=', True)])
     #         rec.visitor_name = self.env['smart_visitor'].search([('is_internal', '=', False)])
+    @api.model
+    def create(self, vals_list):
+        # print(vals_list)
+        # resource_type is 'human' by default. As we are not living in
+        # /r/latestagecapitalism, workcenters are 'material'
+
+        if not vals_list['check_in_time']:
+            print('come here')
+            vals_list['check_in_time'] = datetime.datetime.now()
+            vals_list['check_in_status'] = True
+
+        print(vals_list)
+        print(vals_list['visitor_booked'][0][2])
+        booked_list = self.env['booked.schedule'].search([('date', '=', vals_list['visitor_booked'][0][2]['date']), '|',(
+            'start_time', '>=', vals_list['visitor_booked'][0][2]['start_time']),
+                                                          ('end_time', '<=',
+                                                           vals_list['visitor_booked'][0][2]['end_time'])])
+        print(booked_list)
+        booked_eployees = set()
+        for booked in booked_list:
+            booked_eployees.add(booked.employees.id)
+
+        print(booked_eployees)
+        for rec in vals_list['visitor_booked'][0][2]['employees'][0][2]:
+            if rec in booked_eployees:
+                raise ValidationError("not work")
+
+        print(set(vals_list['visitor_booked'][0][2]['employees'][0][2]))
+
+        # employee_details = self.env['booked.schedule'].search([('visitor_name', 'not in', vals_list['visitor_booked'][0][2]['employees'][0][2])])
+
+        records = super(VisitorCheckInOut, self).create(vals_list)
+        return records
 
     is_internal = fields.Boolean(string='Internal', )
-    visitor_name = fields.Many2one('smart_visitor', string='Visitor Name',domain="[('is_internal','=',is_internal)]")
+    visitor_name = fields.Many2one('smart_visitor', string='Visitor Name', domain="[('is_internal','=',is_internal)]")
     email = fields.Char(related='visitor_name.email', string='Email')
     phone = fields.Char(related='visitor_name.phone_number', string='Phone')
     come_from = fields.Char(string='Come From')
@@ -22,5 +57,5 @@ class VisitorCheckInOut(models.Model):
     check_in_time = fields.Datetime(string="Check in Time")
     check_out_time = fields.Datetime(string="Check out Time")
     image = fields.Binary(string="Image", attachment=True)
-    check_in_status = fields.Boolean(string='Check In Status')
+    check_in_status = fields.Boolean(string='Check In Status', default=False)
     visitor_booked = fields.One2many('booked.schedule', 'visitor_id', string="Visitor")
--- smart_visitor/models/booked.py
+++ smart_visitor/models/booked.py
@@ -8,13 +8,13 @@ class BookedSchedule(models.Model):
         for rec in self:
             return rec.end_time - rec.start_time
 
-    employees = fields.Many2many('smart_visitor', 'employee_rel_booked', string='Employees')
-    visitors = fields.Many2many('smart_visitor', 'visitor_rel_booked', string='Visitors')
+    employees = fields.Many2many('smart_visitor', 'employee_rel_booked', string='Employees',
+                                 domain="[('is_internal','=','true')]")
+    visitors = fields.Many2many('smart_visitor', 'visitor_rel_booked', string='Visitors',
+                                domain="[('is_internal','!=','true')]")
     date = fields.Date(string='Date')
     start_time = fields.Float(string='Start Time')
     end_time = fields.Float(string='End Time')
     duration = fields.Float(string='Duration', compute='cal_duration')
     meeting_note = fields.Text(string='Note')
     visitor_id = fields.Many2one('visitor.check.in.out', string="Visitor Check In")
-
-
